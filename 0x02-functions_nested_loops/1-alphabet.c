fa7b2ced1ed60eb7ff4b8bb00fdae90990fb67cc
Majesty <chibuikeochin@gmail.com>
2022-10-18 04:24:06 -0700

    god

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+main()
+{
+	char lower ='a';
+	void print_alphabet(void);
+
+	while (lower <= z){
+
+		putchar(lower);
+		lower++;
+
+
+	}
+	putchar('\n');
+	return('0');
+
+
+
+
+}
